c79aba8db3476826f2ebb46479cad504c0981496
Dan <s256607@studenti.polito.it>
2021-11-28 19:29:04 +0100

    analyze modified

--- scripts/network_analysis.py
+++ scripts/network_analysis.py
@@ -10,7 +10,10 @@ def analyze_graph(path_to_graph):
     print(nx.info(G))
     degree_dict = dict(G.degree(G.nodes()))
     max_deg = max(degree_dict.values())
+    min_deg = min(degree_dict.values())
+
     print(f"Max degree: {max_deg}")
+    print(f"Min degree: {min_deg}")
     print(f"Avg degree: {statistics.mean(degree_dict.values())}")
 
 
